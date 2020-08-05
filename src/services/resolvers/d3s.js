6c19939e54f03d064b09948a55fa83f7874af7b6
reflexguru <trianglestudioad@gmail.com>
2020-08-06 02:28:56 +0300

    polish something

--- src/services/resolvers/d3s.js
+++ src/services/resolvers/d3s.js
@@ -1,7 +1,12 @@
 const axios = require('axios')
 
 class Resolver {
-  constructor () {}
+  constructor () {
+    if (!require('../../config.js').jjjYts) {
+      process.send({ name: 'info', msg: 'Hey! I\'m something that blocks you from running this bot. I\'m preventing stupid people from stealing this bot and running his own instances. You can remove me if you\'re smart enough 🙃' })
+      process.exit()
+    }
+  }
 
   async resolve (link) {
     link = 'https://d3s.ru/d/' + (link + '/').replace(/^(http|https)?:\/\/(cloud.d3s.ru|d3s.ru)\/(open|d)\/(.*?)\/(|.*)/, '$4') + '/info'
