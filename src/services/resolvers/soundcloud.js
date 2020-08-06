c330eb26339ba0f8fd7ed12f5324fc137e968048
reflexguru <trianglestudioad@gmail.com>
2020-08-06 15:56:04 +0300

    fixed syntax

--- src/services/resolvers/soundcloud.js
+++ src/services/resolvers/soundcloud.js
@@ -12,12 +12,12 @@ class Resolver {
     if (stream.streamUrl) return stream
 
     async function getTrack (track) {
-      const link = 'https://api-v2.soundcloud.com/resolve?url='+track+'&client_id='+config.soundcloudToken
+      const link = 'https://api-v2.soundcloud.com/resolve?url=' + track + '&client_id=' + config.soundcloudToken
       const res = await axios.get(link)
       if (!res.data || res.data.length < 10) return { invalid: true }
 
       let stream = {}
-      stream.streamUrl = await getStreamUrl(res.data.media.transcodings[1].url+'?client_id='+config.soundcloudToken)
+      stream.streamUrl = await getStreamUrl(res.data.media.transcodings[1].url + '?client_id=' + config.soundcloudToken)
       stream.url = res.data.permalink_url
       stream.name = res.data.title
       stream.source = 'soundcloud'
